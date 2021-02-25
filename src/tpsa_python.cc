7b170e415dd1ef187b069fbef4d48f575022f1cb
He Zhang <zhanghe1979@gmail.com>
2021-02-25 05:27:25 -0500

    Add da_set_eps, da_eps, DAVector::clean, DAVector::iszero.

--- src/tpsa_python.cc
+++ src/tpsa_python.cc
@@ -46,7 +46,12 @@ PYBIND11_MODULE(tpsa, m) {
         .def("set_element", (void (DAVector::*)(vector<int>, double)) &DAVector::set_element, "idx"_a, "elem"_a)
         .def("reset", &DAVector::reset)
         .def("reset_const", &DAVector::reset_const, "x"_a)
-        .def("clean", &DAVector::clean, "eps"_a)
+        .def("clean", (void (DAVector::*)()) &DAVector::clean)
+        .def("clean", (void (DAVector::*)(double)) &DAVector::clean, "eps"_a)
+        .def("norm", &DAVector::norm)
+        .def("weighted_norm", &DAVector::weighted_norm, "w"_a)
+        .def("iszero", [](DAVector& v){return v.iszero();})
+        .def("iszero", [](DAVector& v, double eps){return v.iszero(eps);}, "eps"_a)
         .def_static("dim", &DAVector::dim)
         .def_static("order", &DAVector::order)
         .def_static("full_length", &DAVector::full_length)
@@ -87,6 +92,7 @@ PYBIND11_MODULE(tpsa, m) {
     m.def("base", &get_base, py::return_value_policy::reference);
     m.def("assign", [](){return DAVector(0);});
     m.def("assign", [](int n){std::vector<DAVector> v(n); return v;});
+    m.def("da_eps", [](){return DAVector::eps;});
 //    m.def("assign", [](){DAVector x=0; return x;});
     m.def("da_init", &da_init, "Initialize the DA environment.", "da_order"_a, "da_dim"_a, "num_da_vectors"_a);
     m.def("da_clear", &da_clear, "Destroy the DA environment");
@@ -109,6 +115,7 @@ PYBIND11_MODULE(tpsa, m) {
     m.def("da_count", &da_count);
     m.def("da_remain", &da_remain);
     m.def("da_full_length", &da_full_length);
+    m.def("da_set_eps", &da_set_eps);
     m.def("da_der", (void (*)(const DAVector&, unsigned int, DAVector&)) &da_der, "da_vector"_a, "base_id"_a, "da_vector_der"_a);
     m.def("da_int", (void (*)(const DAVector&, unsigned int, DAVector&)) &da_int, "da_vector"_a, "base_id"_a, "da_vector_int"_a);
     m.def("da_der", (DAVector (*)(const DAVector&, unsigned int)) &da_der, "da_vector"_a, "base_id"_a);
